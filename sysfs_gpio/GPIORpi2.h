38fd4568dd4f9b5728857b4a91ed6275981cb40b
Fibona4i <chumaksergiy@gmail.com>
2015-09-27 16:53:48 +0300

    Refactoring code

---
+++ sysfs_gpio/GPIORpi2.h
@@ -0,0 +1,11 @@
+#ifndef RPI2_GPIO_H
+#define RPI2_GPIO_H
+
+#define RPI2_GPIO_7	"4"
+#define RPI2_GPIO_11	"17"
+#define RPI2_GPIO_13	"27"
+#define RPI2_GPIO_8	"14"
+#define RPI2_GPIO_10	"15"
+#define RPI2_GPIO_12	"18"
+
+#endif
